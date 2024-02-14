ee39568298b04ebda3039768185659a99e8bdf94
jesualujan <lujan.oficial@gmail.com>
2024-02-13 19:47:09 -0600

    ✨ Run Node Application

---
+++ app.js
@@ -0,0 +1,5 @@
+// Moment es una libreria de JavaScript tu puedes pasar datos como fechas 
+const moment = require('moment');
+console.log("HELLO, GITHUB DEMO ACTIONS!")
+const date = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(date)
